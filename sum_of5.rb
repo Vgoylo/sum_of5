78e1340798c3f5f86ca404d86f0353a93a97ca4e
vgoylo <y_draiv.by@mail.ru>
2021-06-23 14:06:21 +0300

    Use iterator

--- sum_of5.rb
+++ sum_of5.rb
@@ -1,12 +1,7 @@
 def sum_of5(arr)
-  result = 0
-  arr.each do |element|
-    if element % 5 == 0
-      result += element
-    end
-  end
-
-  result 
+  arr.inject do |sum, n|
+    sum + (n % 5 == 0 ? n : 0)
+  end 
 end
 
 puts sum_of5([100, 122, 25, 5, 20, 44, 64])
